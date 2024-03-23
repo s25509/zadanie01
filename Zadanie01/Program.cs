a4a62c254592a4178976e345d7979ee3047651ab
s25509 <s25509@pjwstk.edu.pl>
2024-03-23 18:19:32 +0100

    new change

--- Zadanie01/Program.cs
+++ Zadanie01/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Goodbye, Cruel World! I've had enough!");
+Console.WriteLine("Goodbye, Cruel World! I've had enough!!!");
 return;
 
 static double CalculateAverage(int[] ints)
