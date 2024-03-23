4c801325a9baf3d23c8303ceff6a92194f988df0
s25509 <s25509@pjwstk.edu.pl>
2024-03-23 17:59:19 +0100

    added function

--- Zadanie01/Program.cs
+++ Zadanie01/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Goodbye, Cruel World! I've had enough!");
+return;
+
+static double CalculateAverage(int[] ints)
+{
+    return ints.Average();
+}
